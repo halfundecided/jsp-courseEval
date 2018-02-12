e30c032aa12bc7f56265c610ead6fb7a0a4bddb0
Mijeong <mban1@stevens.edu>
2018-02-12 00:05:07 -0500

    editted

--- CourseEvaluation/src/util/SHA256.java
+++ CourseEvaluation/src/util/SHA256.java
@@ -2,14 +2,4 @@ package util;
 
 public class SHA256 {
 	
-	public static String getSHA256(String input) {
-		StringBuffor result = new StringBuffer();
-		try {
-			MessageDigest digest = Messagedinest.getInstance("SHA-256");
-			byte[]
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result.toString();
-	}
 }
